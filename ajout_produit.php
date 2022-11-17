d4ec395dd509c45fe83b7a47f623334bd60b0f15
unknown <lucas.chellin@gmail.com>
2022-11-17 15:02:25 +0100

    premiere initiation

---
+++ ajout_produit.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * 4 status de fichier git
+ * -untracked => les fichiers crée mais non suivies par git
+ * -tracked => fichier déja suivi par git mais non envoyé vers le repository
+ * -staged => les fichiers suivi, enregistré mais non envoyé vers le repository
+ * -comited => tous les fichiers envoyés et non modifiés de mon projet
+ */
